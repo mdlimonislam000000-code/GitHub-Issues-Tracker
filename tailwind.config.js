efe487885761dcad884d9eff8ecc61a9c959564c
mdlimonislam000000 <mdlimonislam000000@gmail.com>
2026-03-07 04:06:46 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,3 @@
-//         <div>
-//             <div class="p-4">
-//                 <div class="flex justify-between">
-//                     <img src="./assets/Open-Status.png" alt="">
-//                     <p>high</p>
-//                 </div>
-//                 <div>
-//                     <h3 class="font-semibold text-[#1F2937] mt-3">Fix navigation menu on mobile devices</h3>
-//                     <p class="text-[#64748B] text-[12px] mt-2">The navigation menu doesn't collapse properly on mobile devices...</p>
-//                 </div>
-//                 <div class="flex justify-between mt-3">
-//                     <p>bug</p>
-//                     <p>help wanted</p>
-//                 </div>
-
-//             </div>
-// <hr>
-//             <div class="p-4">
-//                 <p>#1 by john_doe</p>
-//                 <p>1/15/2024</p>
-//             </div>
-//         </div>
 
 
 
