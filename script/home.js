7f08ffde08335818beb67982825ea838fe971738
mdlimonislam000000 <mdlimonislam000000@gmail.com>
2026-03-08 00:08:33 +0600

    all card added

--- script/home.js
+++ script/home.js
@@ -1,3 +1,139 @@
+let allIssues = [];
+
+window.onload = function () {
+    document.getElementById("hero-button-all").style.backgroundColor = "#4A00FF";
+    document.getElementById("hero-button-all").style.color = "white";
+    updateCounter();
+};
+
+let heroBUttonAll = document.getElementById("hero-button-all");
+let heroBUttonOpen = document.getElementById("hero-button-open");
+let heroBUttonClosed = document.getElementById("hero-button-closed");
+
+
+
+function resetStyle() {
+    const buttons = [heroBUttonAll, heroBUttonOpen, heroBUttonClosed];
+    buttons.forEach(function (allBtn) {
+        if (allBtn) {
+            allBtn.style.backgroundColor = "";
+            allBtn.style.color = "";
+        }
+    })
+}
+
+heroBUttonAll.addEventListener("click", function () {
+    resetStyle();
+    this.style.backgroundColor = "#4A00FF";
+    this.style.color = "white";
+    displayIssues(allIssues);
+});
+heroBUttonOpen.addEventListener("click", function () {
+    resetStyle();
+    this.style.backgroundColor = "#4A00FF";
+    this.style.color = "white";
+    const openIssues = allIssues.filter(i => i.status.toLowerCase() === 'open');
+    displayIssues(openIssues);
+});
+heroBUttonClosed.addEventListener("click", function () {
+    resetStyle();
+    this.style.backgroundColor = "#4A00FF";
+    this.style.color = "white";
+    const closedIssues = allIssues.filter(i => i.status.toLowerCase() === 'closed');
+    displayIssues(closedIssues);
+});
+
+
+
+const displayIssues = (issues) => {
+    issuesContainer.innerHTML = "";
+
+    const elements = document.getElementsByClassName("totalCount");
+
+    for (let el of elements) {
+        el.innerText = issues.length;
+    }
+
+    issues.forEach(issue => {
+        const status = issue.status ? issue.status.toLowerCase() : 'open';
+        const borderColor = status === 'open' ? 'border-t-[#00A96E]' : 'border-t-[#A855F7]';
+
+        const card = document.createElement("div");
+        card.className = `card bg-white shadow-xl border-t-4 ${borderColor} p-4`;
+
+        // modal add
+
+        card.setAttribute("onclick", "my_modal_5.showModal()");
+        // const loadIssues = async (id) => {
+        //     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issue/{id}';
+        //     const res = await fetch(url);
+        //     const data = await res.json();
+        //     allIssues = data.data;
+        //     console.log(allIssues);
+        // }
+        // console.log(loadIssues)
+
+
+
+
+
+        // const loadIssues = async (id) => {
+
+        //     const url = https://phi-lab-server.vercel.app/api/v1/lab/issue/${id};
+
+        //     const res = await fetch(url);
+        //     const data = await res.json();
+
+        //     const issue = data.data;
+
+        //     document.getElementById("issueTitle").innerText = issue.title;
+        //     document.getElementById("issueStatus").innerText = issue.status;
+        //     document.getElementById("issueAuthor").innerText = "Opened by " + issue.author;
+        //     document.getElementById("issueDate").innerText = issue.date;
+
+        //     document.getElementById("issueType").innerText = issue.type;
+        //     document.getElementById("issueDescription").innerText = issue.description;
+
+        //     document.getElementById("issueAssignee").innerText = issue.assignee;
+        //     document.getElementById("issuePriority").innerText = issue.priority;
+
+        //     document.getElementById("my_modal_5").showModal();
+        // };
+        
+
+
+        // modal added end
+        card.innerHTML = `
+            <div class="card ">
+            <div class="p-4">
+                <div class="flex justify-between">
+                    <img src="${status === 'open' ? './assets/Open-Status.png' : './assets/Closed- Status .png'}" class="w-5 h-5">
+                    <span class="bg-red-50 text-red-500 text-[10px] font-bold px-2 py-1 rounded-full uppercase">${issue.priority}</span>
+                    
+                </div>
+                <div>
+                    <h3 class="font-semibold text-[#1F2937] mt-3">${issue.title}</h3>
+                    <p class="text-[#64748B] text-[12px] mt-2">${issue.description}</p>
+                </div>
+                <div class="flex  mt-3 space-x-1">
+                    <span class=" bg-red-100 px-2 py-2 text-[12px] rounded-full text-red-600 border border-red-400 ml-1 whitespace-nowrap "><i class="fa-solid fa-bug"></i> ${issue.labels[0]}</span>
+                    <span class="bg-[#d977064d] text-[12px] px-2 py-2 rounded-full text-[#ff5e00]  border border-[#D97706] whitespace-nowrap"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1] ? issue.labels[1] : "No Data Found"}</span>
+                </div>
+
+            </div>
+<hr>
+            <div class="p-4 ">
+                <p>${issue.author}</p>
+                <p class="mt-1">${issue.updatedAt.split("T")[0]}</p>
+            </div>
+        </div>
+        `;
+        issuesContainer.appendChild(card);
+    });
+};
+
+
+
 
 
 const issuesContainer = document.getElementById("issue-container")
@@ -6,8 +142,9 @@ const loadIssues = async () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
     const res = await fetch(url);
     const data = await res.json();
+    allIssues = data.data;
+    displayIssues(allIssues);
 
-    
 
     console.log(data);
     console.log(issuesContainer);
@@ -21,7 +158,7 @@ const loadIssues = async () => {
         card.className = `card bg-white shadow-xl border-t-4 ${borderColor} p-4`;
 
         card.innerHTML = `
-                <div>
+            <div class="card ">
             <div class="p-4">
                 <div class="flex justify-between">
                     <img src="${status === 'open' ? './assets/Open-Status.png' : './assets/Closed- Status .png'}" class="w-5 h-5">
@@ -30,7 +167,7 @@ const loadIssues = async () => {
                 </div>
                 <div>
                     <h3 class="font-semibold text-[#1F2937] mt-3">${allData.title}</h3>
-                    <p class="text-[#64748B] text-[12px] mt-2">${allData.description  }</p>
+                    <p class="text-[#64748B] text-[12px] mt-2">${allData.description}</p>
                 </div>
                 <div class="flex  mt-3 space-x-1">
                     <span class=" bg-red-100 px-2 py-2 text-[12px] rounded-full text-red-600 border border-red-400 ml-1 whitespace-nowrap "><i class="fa-solid fa-bug"></i> ${allData.labels[0]}</span>
@@ -41,12 +178,31 @@ const loadIssues = async () => {
 <hr>
             <div class="p-4">
                 <p>${allData.author}</p>
-                <p>${allData.updatedAt}</p>
+                <p>${allData.updatedAt.split("T")[0]}</p>
             </div>
         </div>
         `;
         issuesContainer.appendChild(card);
     });
-    // issuesContainer.innerHTML = " akane card bosbe"
+
 }
 loadIssues();
+
+
+// document.getElementById("search-btn").addEventListener("click", ()=>{
+//     const input = document.getElementById("input-search")
+//     const searchValue = input.value.trim().toLowerCase();
+//     console.log(searchValue);
+
+//     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+//     .then(res => res.json())
+//     .then(data => {
+//         const allWord = data.data;
+//         console.log(allWord)
+
+//         const filterIssues = allWord.filter(word =>
+//                 word.title.toLowerCase().includes(searchValue) ||
+//                 word.description.toLowerCase().includes(searchValue));
+//         console.log(filterIssues);
+//     })
+// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,78 @@
+// window.onload = function() {
+//     document.getElementById("all-button-hero").style.backgroundColor = "black";
+//     document.getElementById("all-button-hero").style.color = "white";
+//     updateCounter();
+// };
+
+// let firstAllButton = document.getElementById("all-button-hero");
+// let firstInterviewButton = document.getElementById("interview-button-hero");
+// let firstRejectButton = document.getElementById("rejected-button-hero");
+
+// function resetStyle(){
+//     const buttons = [ firstAllButton, firstInterviewButton, firstRejectButton];
+//     buttons.forEach(function(sobBtn){
+//         if(sobBtn){
+//             sobBtn.style.backgroundColor = "";
+//             sobBtn.style.color = "";
+//         }
+//     })
+// }
+
+// firstAllButton.addEventListener("click", function(){
+//     resetStyle();
+//     this.style.backgroundColor = "black";
+//     this.style.color = "white";
+// });
+// firstInterviewButton.addEventListener("click", function(){
+//     resetStyle();
+//     this.style.backgroundColor = "black";
+//     this.style.color = "white";
+// });
+// firstRejectButton.addEventListener("click", function(){
+//     resetStyle();
+//     this.style.backgroundColor = "black";
+//     this.style.color = "white";
+// });
 
 
 
 
 
+    <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+            <div class="p-3">
+                <div>
+                    <h1 class="font-bold text-[1.5rem]">Fix broken image uploads</h1>
+                    <div class="space-x-2">
+                        <span class="bg-[#00A96E] text-white px-2 py-1 rounded-full">Opened</span> <span class="font-bold text-[2rem]">.</span>
+                        <span class="text-[#64748B] text-[12px]">Opened by Fahim Ahmed</span><span class="font-bold text-[2rem]">.</span>
+                        <span class="text-[#64748B] text-[12px]">22/02/2026</span>
+                    </div>
+                </div>
+                <div class="flex  mt-3 space-x-1">
+                    <span class=" bg-red-100 px-2 py-2 text-[12px] rounded-full text-red-600 border border-red-400 ml-1 whitespace-nowrap "><i class="fa-solid fa-bug"></i> BUG</span>
+                    <span class="bg-[#d977064d] text-[12px] px-2 py-2 rounded-full text-[#ff5e00]  border border-[#D97706] whitespace-nowrap"><i class="fa-solid fa-life-ring"></i> HELP WANTED</span>
+                </div>
 
+                <div class="mt-6">
+                    <p class="text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.</p>
+                </div>
+                <div class="flex justify-between items-center p-4 bg-gray-50 rounded-lg mt-5">
+                    <div class=" justify-start">
+                        <p>Assignee:</p>
+                        <h1>Fahim Ahmed</h1>
+                    </div>
+                    <div class="">
+                        <p>Priority:</p>
+                        <span class="bg-red-50 text-red-500 font-bold px-2 py-1 rounded-full uppercase">High</span>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn btn-primary">Close</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
